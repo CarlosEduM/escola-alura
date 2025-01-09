fa59cf27def5b35d18943b921c5951896721540b
CarlosEduardo <carlos.eduardo@icarotech.com>
2025-01-09 10:24:52 -0300

    Configuring permissions

--- escola/views.py
+++ escola/views.py
@@ -2,9 +2,11 @@ from escola.models import Estudante, Curso, Matricula
 from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasEstudanteSerializer, ListaMatriculasCursoSerializer, EstudanteSerializerV2
 from rest_framework import viewsets, generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.throttling import UserRateThrottle
+from escola.throttles import MatriculaAnonRateThrottle
 
 class EstudanteViewSet(viewsets.ModelViewSet):
-    queryset = Estudante.objects.all()
+    queryset = Estudante.objects.all().order_by('id')
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['nome']
     search_fields = ['nome', 'cpf']
@@ -16,22 +18,24 @@ class EstudanteViewSet(viewsets.ModelViewSet):
         return EstudanteSerializer
     
 class CursoViewSet(viewsets.ModelViewSet):
-    queryset = Curso.objects.all()
+    queryset = Curso.objects.all().order_by('id')
     serializer_class = CursoSerializer
 
 class MatriculaViewSet(viewsets.ModelViewSet):
-    queryset = Matricula.objects.all()
+    queryset = Matricula.objects.all().order_by('id')
     serializer_class = MatriculaSerializer
+    throttle_classes = [UserRateThrottle, MatriculaAnonRateThrottle]
+    http_method_names = ['get', 'post']
 
 class ListaMatriculasEstudante(generics.ListAPIView):
     def get_queryset(self):
-        return Matricula.objects.filter(estudante_id= self.kwargs['pk'])
+        return Matricula.objects.filter(estudante_id=self.kwargs['pk']).order_by('id')
     
     serializer_class = ListaMatriculasEstudanteSerializer
 
 class ListaMatriculasCurso(generics.ListAPIView):
     def get_queryset(self):
-        return Matricula.objects.filter(curso_id= self.kwargs['pk'])
+        return Matricula.objects.filter(curso_id=self.kwargs['pk']).order_by('id')
     
     serializer_class = ListaMatriculasCursoSerializer
 
